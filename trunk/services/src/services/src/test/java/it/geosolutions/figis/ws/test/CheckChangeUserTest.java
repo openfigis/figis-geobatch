d77db1a12204d824455f43eba12ec4ba22ec3fbb
Riccardo Galiberti <Riccardo@Riccardo-PC.(none)>
2011-12-17 18:35:32 +0100

    test CredentialManager corretto

--- trunk/services/src/services/src/test/java/it/geosolutions/figis/ws/test/CheckChangeUserTest.java
+++ trunk/services/src/services/src/test/java/it/geosolutions/figis/ws/test/CheckChangeUserTest.java
@@ -37,34 +37,18 @@
 
 
 
-import it.geosolutions.figis.model.Intersection;
-import it.geosolutions.figis.model.Intersection.Status;
-import it.geosolutions.figis.persistence.dao.ConfigDao;
-import it.geosolutions.figis.persistence.dao.IntersectionDao;
-import it.geosolutions.figis.requester.Request;
-import it.geosolutions.figis.security.IntersectionEngineAuthenticationInterceptor;
+import it.geosolutions.figis.model.User;
 import it.geosolutions.figis.security.authentication.CredentialsManager;
-import it.geosolutions.figis.ws.FigisService;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.util.List;
-import java.util.Properties;
 
 import junit.framework.TestCase;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class CheckChangeUserTest extends TestCase
@@ -72,178 +56,113 @@ public class CheckChangeUserTest extends TestCase
 
     static final Logger LOGGER = Logger.getLogger(CheckChangeUserTest.class.toString());
 
-    FigisService figisService = null;
-	ConfigDao configDao = null;
-	IntersectionDao intersectionDao = null;
-	String ieServiceUsername = "admin";
-	String ieServicePassword = "abramisbrama";
-	IntersectionEngineAuthenticationInterceptor ieAuthInterceptor = null;
-	CredentialsManager userCheckUtils = null;
-	//static final String HOST = "http://localhost:8082";
-	static final String HOST = "http://localhost:9001";
-	static final String IE_SERV_CONF = "/ie-services/config";
+    CredentialsManager userCheckUtils = null;
 	/*constants for changing test properties file*/
 	private static final String TEST_PROPERTIES_FILE = "test_userac.properties";
+	private static final String TO_TEST_PROPERTIES_FILE = "PROPERTIES_FILE=src/test/resources/"+TEST_PROPERTIES_FILE;
 	private static final String TEST_USERS_ROLE_ADMIN_USER = "admin";
 	private static final String TEST_USERS_ROLE_ADMIN_PASSWORD = "abramisbrama";
 	private static final String SEPARATOR = "@";
-
+	long PERIOD = 30000;
+	
 	private static final String TEST_USERS_ROLE_ADMIN = "usersRoleAdmin="+
 								TEST_USERS_ROLE_ADMIN_USER+SEPARATOR+TEST_USERS_ROLE_ADMIN_PASSWORD;
 
-	private static final String TEST_USERS_ROLE_USER = "usersRoleUser=pippo@pippo";
+	
 
 	private static final String TEST_USERS_ROLE_ADMIN_USER_MODIFIED = "admin";
 	private static final String TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED = "abramis";
 	private static final String TEST_USERS_ROLE_ADMIN_MODIFIED = "usersRoleAdmin="+
 								TEST_USERS_ROLE_ADMIN_USER_MODIFIED+SEPARATOR+TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED;
-	private static final long TEST_PERIOD = 30000;
+	private static final String TO_TEST_USERS_ROLE_USER = "usersRoleUser=pippo@pippo";
+	private static final String TO_TEST_PERIOD = "checkPeriod=30000";
 	
-	long PERIOD = 30000;
-	Intersection int1 = null;
 
-   
-    @Before
-    public void setUp() throws Exception
-    {
-        try{
-	        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContextTestChangeUser.xml");
-	        //configDao = (ConfigDao) ctx.getBean("ie-configDAO");
-	        //intersectionDao = (IntersectionDao) ctx.getBean("ie-intersectionDAO");
-	        //figisService = (FigisService) ctx.getBean("figisServiceImpl");
-	        ieAuthInterceptor  = (IntersectionEngineAuthenticationInterceptor) ctx.getBean("ieAuthInterceptor");
-	        userCheckUtils = (CredentialsManager)ctx.getBean("ieUsersCheckUtils");
-	        //Request.initConfig();
-			//Request.initIntersection();
-	        Intersection int1 = new Intersection(true, true, true,"srcLayer", "trgLayer", "srcCodeField",
-	    			"trgCodeField", "maskLayer", "areaCRS", Status.TOCOMPUTE);
-        }catch(Exception e){
-        	LOGGER.error("FAIL SETUP:",e);
-        }
-    }
-
+	
+	/**
+	 * test for class Credential manager: it test funtionality about reloading file ad 
+	 * denied access to user with old password.
+	 * 
+	 * @throws IOException
+	 */
     @Test
     public void test_CheckUserTest() throws IOException
     {
     	LOGGER.info("START TEST");
-    	long l = -1;
     	try{
     		if(userCheckUtils==null){
-    			//createUseracTestFile(PROPERTIES_TEST_FILE);
-    			userCheckUtils = new CredentialsManager(TEST_PROPERTIES_FILE, TEST_USERS_ROLE_ADMIN, TEST_USERS_ROLE_USER, PERIOD);
-    			modifyUseracTestFile(TEST_PROPERTIES_FILE);
-    			userCheckUtils.reload();
-    			Request.initIntersection();
-    	        Request.initConfig();
-    			l = Request.insertIntersection(HOST, int1, TEST_USERS_ROLE_ADMIN_USER_MODIFIED, TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED);
-    		}else{
-    			Request.initIntersection();
-    	        Request.initConfig();
-    			l = Request.insertIntersection(HOST, int1, TEST_USERS_ROLE_ADMIN_USER_MODIFIED, TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED);
-    			if(l==0){
-    				assertFalse(true);
+    			modifyUseracTestFile(TEST_PROPERTIES_FILE,TEST_USERS_ROLE_ADMIN);
+    			LOGGER.info("created test_userac.properties with admin password: "+TEST_USERS_ROLE_ADMIN);
+    			userCheckUtils = new CredentialsManager(TEST_PROPERTIES_FILE, TEST_USERS_ROLE_ADMIN, TO_TEST_USERS_ROLE_USER, PERIOD);
+    			User user = userCheckUtils.getUser(TEST_USERS_ROLE_ADMIN_USER, TEST_USERS_ROLE_ADMIN_PASSWORD);
+    			if(user.getPassword().equals(TEST_USERS_ROLE_ADMIN_PASSWORD)){
+    				assertTrue(true);
+    				LOGGER.info("test access with password '"+TEST_USERS_ROLE_ADMIN_PASSWORD+"': access");
+    			}else{
+    				assertTrue(false);
+    				LOGGER.info("test access with password '"+TEST_USERS_ROLE_ADMIN_PASSWORD+"': access failed");
     			}
+    			modifyUseracTestFile(TEST_PROPERTIES_FILE,TEST_USERS_ROLE_ADMIN_MODIFIED);
+    			LOGGER.info("created test_userac.properties with admin password: "+TEST_USERS_ROLE_ADMIN_MODIFIED);
+    			userCheckUtils.reload();
+    			try{
+    				user = null;
+    				user = userCheckUtils.getUser(TEST_USERS_ROLE_ADMIN_USER_MODIFIED, TEST_USERS_ROLE_ADMIN_PASSWORD);
+    			}catch(Exception e){
+    	       		LOGGER.info("test access with old password '"+TEST_USERS_ROLE_ADMIN_PASSWORD+"': access denied, password has been changed, test ok",e);
+    	       		assertTrue(true);
+    		       }
+    			if(user!=null && user.getPassword().equals(TEST_USERS_ROLE_ADMIN_PASSWORD)){
+    				LOGGER.info("test access with password '"+TEST_USERS_ROLE_ADMIN_PASSWORD+"': access");
+    				assertTrue(false);
+    			}
+    		}else{
+    			modifyUseracTestFile(TEST_PROPERTIES_FILE,TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED);
     			userCheckUtils.reload();
     			
-    			l = Request.insertIntersection(HOST, int1, TEST_USERS_ROLE_ADMIN_USER_MODIFIED, TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED);
-    			if(l==-1){
-    				assertFalse(true);
-    			}
-    			l = Request.insertIntersection(HOST, int1, TEST_USERS_ROLE_ADMIN_USER_MODIFIED, TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED);
-    			if(l==-1){
-    				assertFalse(true);
+    			User user = userCheckUtils.getUser(TEST_USERS_ROLE_ADMIN_USER_MODIFIED, TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED);
+    			if(user.getPassword().equals(TEST_USERS_ROLE_ADMIN_PASSWORD_MODIFIED)){
+    				assertTrue(true);
+    			}else{
+    				assertTrue(false);
     			}
     		}
-			assertTrue(l!=-1 && l >= 0);
 	   }catch(Exception e){
        		LOGGER.error("FAIL testInsertConfig:",e);
        }
     }
     
-   
-    
-    
-    public void modifyUseracTestFile(String useracProptestFile) throws IOException{
-    	FileWriter fstream = null;
-    	BufferedWriter out = null;
-    	Properties props = new Properties();
-        InputStream pin = null;
-		try {
-			//pin = new BufferedInputStream(new FileInputStream(useracProptestFile));
-			java.net.URL url = this.getClass().getClassLoader().getResource(useracProptestFile);
-			
-			File   propertyFile = new File(url.toURI());
-		        if(!propertyFile.isFile()||!propertyFile.canRead()||propertyFile.isHidden()){
-		        	throw new IllegalArgumentException("Unable to reach file:"+url.toURI().toString()+"\n"+
-		        			"canRead:"+propertyFile.canRead()+",isFile:"+propertyFile.isFile()+",isHidden:"+propertyFile.isHidden()
-		        			);
-		        }
-		        pin = new BufferedInputStream(new FileInputStream(propertyFile));
-			if(LOGGER.isTraceEnabled()){
-	        	LOGGER.trace("READING TEST_PROPERTIES_FILE " + useracProptestFile);
-	        }
-			
-		    props.load(pin);
-
-		    if(LOGGER.isTraceEnabled()){
-	        	LOGGER.trace("PROPERTIES_FILE reloaded: " + useracProptestFile);
-	        }
-		}catch (IOException e) {
-		    throw new IOException("UsersCheckUtils: error on reading TEST file properties: PROPERTIES_FILE: " + useracProptestFile);
-		} catch (URISyntaxException e) {
-			LOGGER.trace(e.getLocalizedMessage(),e);
-		}finally{
-			if(pin!=null){
-				IOUtils.closeQuietly(pin);
-			}
-		}
-		
-    	try{
-    		  // Create file 
-    		  fstream = new FileWriter(useracProptestFile);
-    		  out = new BufferedWriter(fstream);
-    		  out.write(TEST_PROPERTIES_FILE+"\n");
-    		  out.write(TEST_PERIOD+"\n");
-    		  out.write(TEST_USERS_ROLE_ADMIN_MODIFIED+"\n");
-    		  out.write(TEST_USERS_ROLE_USER+"\n");
-		  }catch (Exception e){//Catch exception if any
-			  LOGGER.error(e.getLocalizedMessage(),e);
-		  }finally{
-			//Close the output stream
-			  if(fstream!=null){
-				  IOUtils.closeQuietly(fstream);
-			  }
-			  if(out!=null){
-					IOUtils.closeQuietly(out);
-			  }
-		  }
-    	}
-    
-    public void createUseracTestFile(String useracProptestFile){
+    /**
+     * Create or modified test file
+     * 
+     * @param useracProptestFile
+     * @param userRoleAdminPwd
+     * @throws IOException
+     */
+    public void modifyUseracTestFile(String useracProptestFile, String userRoleAdminPwd) throws IOException{
+    	
     	FileWriter fstream = null;
     	BufferedWriter out = null;
     	try{
-    		  // Create file 
-    		  fstream = new FileWriter(useracProptestFile);
-    		  out = new BufferedWriter(fstream);
-    		  out.write(TEST_PROPERTIES_FILE+"\n");
-    		  out.write("checkPeriod="+TEST_PERIOD+"\n");
-    		  out.write(TEST_USERS_ROLE_ADMIN_MODIFIED+"\n");
-    		  out.write(TEST_USERS_ROLE_USER+"\n");
-    		  out.flush();
-		  }catch (Exception e){//Catch exception if any
-			  LOGGER.error(e.getLocalizedMessage(),e);
-		  }finally{
-			//Close the output stream
-			  if(out!=null){
-				  IOUtils.closeQuietly(out);
-			  }
-			  if(fstream!=null){
-				  IOUtils.closeQuietly(fstream);
-			  }
-			  
+    	  // Create file 
+    	  java.net.URL url = this.getClass().getClassLoader().getResource(useracProptestFile);
+		  fstream = new FileWriter(url.toURI().toURL().getPath());
+		  out = new BufferedWriter(fstream);
+		  out.write(TO_TEST_PROPERTIES_FILE+"\n");
+		  out.write(TO_TEST_PERIOD+"\n");
+		  out.write(userRoleAdminPwd+"\n");
+		  out.write(TO_TEST_USERS_ROLE_USER+"\n");
+		  out.flush();
+	  }catch (Exception e){//Catch exception if any
+		  LOGGER.error(e.getLocalizedMessage(),e);
+	  }finally{
+		//Close the output stream
+		  if(fstream!=null){
+			  IOUtils.closeQuietly(fstream);
 		  }
-    	}
-    
-    
+		  if(out!=null){
+				IOUtils.closeQuietly(out);
+		  }
+	  }
+	}
 }
