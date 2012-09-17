8bfc2d63f14de2717494c70de477f904fe78a44a
Simone Giannecchini <simone.giannecchini@geo-solutions.it>
2012-09-17 19:38:25 +0200

    started to clean-up structure

--- branches/0.6.x/ie-web/src/main/java/it/geosolutions/figis/client/security/PageAuthorizer.java
+++
@@ -1,9 +0,0 @@
-package it.geosolutions.figis.client.security;
-
-import org.acegisecurity.Authentication;
-
-public interface PageAuthorizer {
-
-	@SuppressWarnings("unchecked")
-	public boolean isAccessAllowed(Class pageClass, Authentication authentication);
-}
